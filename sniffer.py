097fafdf3129f9fed2e1cc6be1851c2a7d99875f
meijiishinislame <zacharysilvadev@gmail.com>
2022-12-21 19:43:06 -1000

    fixe dns code

--- sniffer.py
+++ sniffer.py
@@ -15,11 +15,23 @@ def add_to_database(query, args):
 	conn.commit()
 	conn.close()
 
+def get_from_database(query, args):
+	conn = sqlite3.connect(database)
+	cursor = conn.cursor()
+	cursor.execute(query, args)
+	results = cursor.fetchall()
+	conn.commit()
+	conn.close()
+	return results
+
+def ip_in_database_already(ip_address):
+	result = get_from_database("SELECT * FROM ipinfo WHERE ip=?",(ip_address,))
+	return True if result else False
 
 while True:
 	try:
-		# get 10 pkts at a time on the RADMIN VPN interface (ips of players playing the game are udp)
-		pkts = sniff(filter="udp", count=100, iface="Radmin VPN")
+		# get 100 pkts at a time on the RADMIN VPN interface (ips of players playing the game are udp)
+		pkts = sniff(count=100, iface="Radmin VPN")
 
 		#get source ip from packet
 		for pkt in pkts:
@@ -29,20 +41,23 @@ while True:
 				dns = None
 
 				#get dns info if applicable
-				if pkt.haslayer("DNS") and pkt["DNS"].qr == 0: dns = pkt["DNS"].qd.qname 
+				if pkt.haslayer("DNS") and pkt["DNS"].an: 
+					dns = pkt["DNS"].an.rdata.decode("utf-8").split("::")[0]
 
 				#get ips of people who opted out of being pinged
+				opted_out_ips = ""
 				with open(opted_out_ips_file, "r") as f:
 					opted_out_ips = f.readlines()
 
-					# Check if the target ip is in the list of ip_list
-					if ip not in opted_out_ips:
-						if dns:
-							add_to_database("INSERT OR REPLACE INTO ipinfo(ip, dns_name) VALUES (?, ?)", (ip, dns))
-							print("IP", ip, "with DNS", dns, "added.")
-						else:
-							add_to_database("INSERT OR REPLACE INTO ipinfo(ip, dns_name) VALUES (?, ?)", (ip, "no_dns_info"))
-							print("IP", ip, "added.")
+				# Check if the target ip is in the list of ip_list
+				if ip not in opted_out_ips:
+					#If theres a DNS entry we want to update the old one
+					if dns:
+						add_to_database("INSERT OR REPLACE INTO ipinfo(ip, dns_name) VALUES (?, ?)", (ip, dns))
+						print("IP", ip, "with DNS", dns, "added.")
+					else:
+						add_to_database("INSERT INTO ipinfo(ip, dns_name) VALUES (?, ?)", (ip, "no_dns_info"))
+						print("IP entry", ip, "added with no dns info")
 
 		time.sleep(10)
 
